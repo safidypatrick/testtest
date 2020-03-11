4b517dded30ae200850df49fd21d3918b0d1f64a
safidypatrick <safidypatrick8gmail.com>
2020-03-11 13:22:50 +0300

    qf

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,20 @@
+class ArticlesController < ApplicationController
+  def index
+    @article = Article.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
